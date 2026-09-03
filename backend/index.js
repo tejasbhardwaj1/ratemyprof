f2228a1d4dad6a8b3ffb6ec23793053360d9e9bf
Tejas Bhardwaj <tejasbhardwaj024@gmail.com>
2026-09-03 12:14:35 +0530

    Make backend deploy-ready: dotenv, dynamic PORT, env vars, start script

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const supabase = require('./supabaseClient');
@@ -102,6 +103,5 @@ app.get("/", (req, res) => {
   res.send("Backend is running");
 });
 
-app.listen(5001, () => {
-  console.log('Server running on port 5001');
-});
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => console.log('Server running on port ' + PORT));
